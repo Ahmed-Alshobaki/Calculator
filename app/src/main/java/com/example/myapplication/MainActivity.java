11203a245d4d0bd58dd8f5451b8230aefa9e9683
Ahmed-Alshobaki <ahmedzon0598596843@gmail.com>
2023-03-02 14:23:33 +0200

    format

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,46 +9,46 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button  b_pls;
-       double all ;
-    String S_all ="=";
-            TextView seen;
+    Button b_pls;
+    double all;
+    String S_all = "=";
+    TextView seen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-       seen = findViewById(R.id.seen);
-        Button  b1= findViewById(R.id.button1);
-        Button  b2= findViewById(R.id.button2);
-        Button  b3= findViewById(R.id.button3);
-        Button  b4= findViewById(R.id.button4);
-        Button  b5= findViewById(R.id.button5);
-        Button  b6= findViewById(R.id.button6);
-        Button  b7= findViewById(R.id.button7);
-        Button  b8= findViewById(R.id.button8);
-        Button  b9= findViewById(R.id.button9);
-      b_pls= findViewById(R.id.buttonpls);
-        Button b_minus =findViewById(R.id.buttonminus);
-        Button  b_hit= findViewById(R.id.buttonhit);
-        Button  b_fseam= findViewById(R.id.buttondestiny);
-        Button  b_End= findViewById(R.id.End);
-        Button  b_delete= findViewById(R.id.buttondelete);
+        seen = findViewById(R.id.seen);
+        Button b1 = findViewById(R.id.button1);
+        Button b2 = findViewById(R.id.button2);
+        Button b3 = findViewById(R.id.button3);
+        Button b4 = findViewById(R.id.button4);
+        Button b5 = findViewById(R.id.button5);
+        Button b6 = findViewById(R.id.button6);
+        Button b7 = findViewById(R.id.button7);
+        Button b8 = findViewById(R.id.button8);
+        Button b9 = findViewById(R.id.button9);
+        b_pls = findViewById(R.id.buttonpls);
+        Button b_minus = findViewById(R.id.buttonminus);
+        Button b_hit = findViewById(R.id.buttonhit);
+        Button b_fseam = findViewById(R.id.buttondestiny);
+        Button b_End = findViewById(R.id.End);
+        Button b_delete = findViewById(R.id.buttondelete);
 
 
         findViewById(R.id.point).setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
-                seen.setText(seen.getText().toString()+".");
+                seen.setText(seen.getText().toString() + ".");
             }
         });
-      findViewById(R.id.Remainder).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.Remainder).setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
-                seen.setText(seen.getText().toString()+"%");
+                seen.setText(seen.getText().toString() + "%");
             }
         });
 
@@ -56,21 +56,21 @@ public class MainActivity extends AppCompatActivity {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
-                seen.setText(seen.getText().toString()+"/");
+                seen.setText(seen.getText().toString() + "/");
             }
         });
-       b_delete.setOnClickListener(new View.OnClickListener() {
+        b_delete.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
-               all=0.0;
+                all = 0.0;
             }
         });
         b_minus.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
-                seen.setText(seen.getText().toString()+"-");
+                seen.setText(seen.getText().toString() + "-");
             }
         });
 
@@ -78,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
-                seen.setText(seen.getText().toString()+"*");
+                seen.setText(seen.getText().toString() + "*");
             }
 
 
@@ -89,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
-                seen.setText(seen.getText().toString()+"+");
+                seen.setText(seen.getText().toString() + "+");
             }
 
 
@@ -100,49 +100,49 @@ public class MainActivity extends AppCompatActivity {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
-             seen.setText(seen.getText().toString()+"1");
+                seen.setText(seen.getText().toString() + "1");
             }
         });
         b2.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
-               seen.setText(seen.getText().toString()+"2");
+                seen.setText(seen.getText().toString() + "2");
             }
         });
         b3.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
-               seen.setText(seen.getText().toString()+"3");
+                seen.setText(seen.getText().toString() + "3");
             }
         });
         b4.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
-               seen.setText(seen.getText().toString()+"4");
+                seen.setText(seen.getText().toString() + "4");
             }
         });
         b5.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
-               seen.setText(seen.getText().toString()+"5");
+                seen.setText(seen.getText().toString() + "5");
             }
         });
         b6.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
-               seen.setText(seen.getText().toString()+"6");
+                seen.setText(seen.getText().toString() + "6");
             }
         });
         b7.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
-               seen.setText(seen.getText().toString()+"7");
+                seen.setText(seen.getText().toString() + "7");
             }
 
 
@@ -151,7 +151,7 @@ public class MainActivity extends AppCompatActivity {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
-                seen.setText(seen.getText().toString()+"8");
+                seen.setText(seen.getText().toString() + "8");
             }
 
 
@@ -160,7 +160,7 @@ public class MainActivity extends AppCompatActivity {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
-                seen.setText(seen.getText().toString()+"9");
+                seen.setText(seen.getText().toString() + "9");
             }
 
 
@@ -168,7 +168,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.zero).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                seen.setText(seen.getText().toString()+0);
+                seen.setText(seen.getText().toString() + 0);
             }
         });
 
@@ -182,27 +182,21 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-   @SuppressLint("SetTextI18n")
-   public void  opr (String new_opr){
-        switch (S_all){
-            case "=" :
-                all=Double.parseDouble(seen.getText().toString());
+    @SuppressLint("SetTextI18n")
+    public void opr(String new_opr) {
+        switch (S_all) {
+            case "=":
+                all = Double.parseDouble(seen.getText().toString());
 
-            case  "+":
-                all+=Double.parseDouble(seen.getText().toString());
-
-
+            case "+":
+                all += Double.parseDouble(seen.getText().toString());
 
 
         }
-                S_all=new_opr;
-            seen.setText(all+"");
+        S_all = new_opr;
+        seen.setText(all + "");
 
     }
 
 
-
-
-
-
-    }
+}
